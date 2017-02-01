7f800e644b282ad57bd12263a39b314f669b659f
Andreas <aks1992@gmail.com>
2017-02-01 10:19:48 +0100

    Add server

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+//onsole.log("Hello world!");
+
+var http = require("http");
+
+http.createServer(function (request, response) {
+  //Send HTTP header
+  // HTTP status: 200 : OK
+  // Content type: text/plain
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Send the response body as "Hello World"
+  response.end('Hello World\n');
+}).listen(8081);
+
+console.log('Server running at http://127.0.0.1:8081/');
